c8e7408594428ee84a930d874b18c1da04364b8c
Suzanne Banasihan <sjb04@ymail.com>
2023-01-10 21:41:18 -0800

    Added code to Drinkitems.js and Merchandise.js

--- server/models/Drinkitems.js
+++ server/models/Drinkitems.js
@@ -1,1 +1,22 @@
-//Drink
+const { Schema } = require('mongoose');
+
+const drinkItemSchema = new Schema({
+  name: 
+    {
+      type: String,
+      require: true,
+    },
+  description: {
+    type: String,
+    required: true,
+  },
+  drinkSId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
+
+module.exports = drinkItemSchema;
--- server/models/Merchandise.js
+++ server/models/Merchandise.js
@@ -1,1 +1,22 @@
-//merchandise
+const { Schema } = require('mongoose');
+
+const merchandiseSchema = new Schema({
+  name: 
+    {
+      type: String,
+      require: true,
+    },
+  description: {
+    type: String,
+    required: true,
+  },
+  merchandiseId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
+
+module.exports = merchandiseSchema;
