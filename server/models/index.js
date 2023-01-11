8a0d1f30f05b77cc0fd25130c68e73cd6c67cf8a
Rima Das <rimadas.automationtester@gmail.com>
2023-01-10 20:08:34 -0800

    Adding index.js, Drinkitems.js and Merchandise.js files

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const FoodItem = require('./Fooditems');
+const DrinkItem = require('./Drinkitems');
+const Feedback = require('./Feedback');
+const Merchandise = require('./Merchandise');
+const TheTeam = require('./TheTeam')
+
+module.exports = { User, FoodItem, DrinkItem,  Feedback, Merchandise, TheTeam};
