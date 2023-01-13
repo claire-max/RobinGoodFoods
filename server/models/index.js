3678abf362de3027baff5415886faa6fb07e837b
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2023-01-12 19:36:33 -0800

    adding schema changes

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-const User = require('./User');
-const FoodItem = require('./Fooditems');
-const DrinkItem = require('./Drinkitems');
-const Feedback = require('./Feedback');
-const Merchandise = require('./Merchandise');
-const TheTeam = require('./TheTeam')
-
-module.exports = { User, FoodItem, DrinkItem,  Feedback, Merchandise, TheTeam};
