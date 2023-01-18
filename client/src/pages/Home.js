46d2772c95078cc99686add9d1d9b590e0246131
Rima Das <rimadas.automationtester@gmail.com>
2023-01-17 18:37:41 -0800

    Updating the Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,14 @@ import { Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import '../App.css'
 import ProductList from "../components/ProductList"
+import CategoryMenu from "../components/CategoryMenu";
 import Footer from "../components/Footer"
 
 const Home = () => {
   return (
     // <div className="container">
       <div>
-        <ProductList />
-       
+        <CategoryMenu />     
         <Footer/>
       </div>
      
