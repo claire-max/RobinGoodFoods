bf16cbb5244fd5b67a93ab9bf7d329a9eda6e971
Rima Das <rimadas.automationtester@gmail.com>
2023-01-17 19:56:13 -0800

    Products Seeding successfully

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import '../App.css'
-import ProductList from "../components/ProductList"
 import CategoryMenu from "../components/CategoryMenu";
 import Footer from "../components/Footer"
+
 
 const Home = () => {
   return (
     // <div className="container">
       <div>
         <CategoryMenu />     
-        <Footer/>
+        <Footer/>       
       </div>
      
    
